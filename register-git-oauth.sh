22dc4457231ebe2bbbdc8140a43157f4effe6f82
nelt <nel.taurisson@gmail.com>
2015-02-10 17:02:09 +0100

    touched in current dir

--- register-git-oauth.sh
+++ register-git-oauth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
 
-touch target/git-credentials.txt
-git config credential.helper "store --file=target/git-credentials.txt"
-echo "https://${GH_TOKEN}:@github.com" > target/git-credentials.txt
+touch git-credentials.txt
+git config credential.helper "store --file=git-credentials.txt"
+echo "https://${GH_TOKEN}:@github.com" > git-credentials.txt
