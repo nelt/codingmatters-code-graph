86e18ec9a75d21df994b7c49d1d746ca8332361d
nelt <nel.taurisson@gmail.com>
2015-02-10 16:15:00 +0100

    secured git push fix3

--- register-git-oauth.sh
+++ register-git-oauth.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
+touch target/git-credentials
 git config credential.helper "store --file=target/git-credentials"
 echo "https://${GH_TOKEN}:@github.com" > target/git-credentials
