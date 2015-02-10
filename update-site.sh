243153b62126dd20314ad65e587d66d01aef474d
nelt <nel.taurisson@gmail.com>
2015-02-10 14:37:58 +0100

    fixed maven-helper-plugin version in pom

--- update-site.sh
+++ update-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-set VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -o | grep -Ev '(^\[|Download\w+:)')
+VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -o | grep -Ev '(^\[|Download\w+:)')
 
 echo "updating site for version $VERSION"
 
@@ -17,8 +17,8 @@ HERE=$(pwd)
 cd target/gh-pages
 
 git add jacoco-coverage/$VERSION 
-git commit -a -m "update site with jacoco coverage for version $VERSION"
-git push
+#git commit -a -m "update site with jacoco coverage for version $VERSION"
+#git push
 
 cd $HERE
 echo "done."
